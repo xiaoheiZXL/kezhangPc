537f9137b41a02542b687774b52e8b2f7bd6d711
zhaoxinlei <991807272@qq.com>
2018-04-01 17:20:33 +0800

    #修改打包信息#

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import authorWork from '../components/subcom/author-work.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: './',
   routes: [
     {
       path: '/item/:stampId',
