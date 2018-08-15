a45162ab82d453df4cec528a66d3def1a6c2ccf5
zhaoxinlei <991807272@qq.com>
2018-08-15 22:01:27 +0800

    #修改搜索逻辑#

--- src/components/subcom/search.vue
+++ src/components/subcom/search.vue
@@ -1,175 +1,12 @@
-<style scoped>
-  .container {
-    box-sizing: border-box;
-  }
-
-  .search {
-    /*background-color: pink;*/
-    background: url('../../../static/images/search-bg.png') center center no-repeat;
-
-  }
-
-  .search .left {
-    /*width:.75rem;*/
-    /*height:1rem;*/
-    background: url('../../../static/images/result-left-bg.png') center center no-repeat;
-  }
-
-  .search .right {
-    text-align: center;
-    background: url('../../../static/images/search-box.png') center center no-repeat;
-  }
-
-  /*中间*/
-  .search .center {
-    position: relative;
-  }
-
-  .search .center.searched {
-    background: url('../../../static/images/search-bg2.png') center center no-repeat;
-  }
-
-  textarea {
-    padding-top: .2rem;
-  }
-
-  button {
-    color: transparent;
-  }
-
-  /*名家列表*/
-  .searchbtn {
-    background: url('../../../static/images/search-btn.png') center center no-repeat;
-    cursor: pointer;
-  }
-
-  /*搜索按钮*/
-  .authorlist {
-    background: url('../../../static/images/author.png') center center no-repeat;
-    cursor: pointer;
-  }
-
-  /*名家列表*/
-  .outer-box {
-    background-color: #baa99a;
-    position: absolute;
-    /*left: 0;*/
-    overflow: hidden;
-  }
-
-  .author-list {
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: row-reverse;
-    flex-wrap: wrap;
-    overflow-y: scroll;
-    box-sizing: border-box;
-  }
-
-  /* for Chrome */
-  .author-list::-webkit-scrollbar {
-    display: none;
-  }
-
-  .author-item {
-    box-sizing: border-box;
-    padding-top: .2rem;
-    font-size: .3rem;
-    font-family: 'FangSong_GB2312';
-    border: 1px solid #fff;
-    margin-right: -1px;
-    margin-top: -1px;
-    cursor: pointer;
-    color: #fff;
-  }
-
-  .author-item.active {
-    color: #a38e70;
-    background-color: #fff;
-  }
-
-  .search-result {
-    /*background-color: rgba(0,0,0,.4);*/
-  }
-
-  .search-result .inner-box {
-    background-color: rgba(0, 0, 0, .4);
-  }
-
-  .search-result > ul {
-    height: 100%;
-  }
-
-  .stamp-item {
-    width: 33%;
-    height: 50%;
-    box-sizing: border-box;
-    font-size: 14px;
-  }
-
-  .stamp-item .top {
-    width: 100%;
-    height: 50%;
-    overflow: hidden;
-    box-sizing: border-box;
-    padding: 40px 20px;
-  }
-
-  .stamp-item img {
-    width: 100%;
-    display: block;
-    margin: 0 auto;
-  }
-
-   .stamp-item p {
-     width: 20%;
-     margin: 0px auto;
-     font-family: 'FangSong_GB2312';
-     font-size: 18px;
-     line-height: 30px;
-     text-overflow: ellipsis;
-     display: -webkit-box;
-     -webkit-box-orient: vertical;
-     -webkit-line-clamp: 4;
-
-     overflow: hidden;
-
-      position:relative;
-      /*line-height:1.4em;*/
-      /* 3 times the line-height to show 3 lines */
-      height:112px;
-      overflow:hidden;
-   }
-  .stamp-item p::after {
-       content:"...";
-       font-weight:bold;
-       position:absolute;
-       bottom:0;
-       right:0;
-       padding:0 20px 1px 45px;
-   }
-
-  .input-area {
-    color: #a38e70;
-    font-family: 'FangSong_GB2312';
-  }
-
-  .home-btn {
-    background: url('../../../static/images/home-btn.png') no-repeat center center;
-    position: absolute;
-    z-index: 99999;
-    /*left: 0;*/
-  }
-</style>
 <template>
-  <div class="container" :style="{'width':containerWidth+'px',
+  <div
+    class="container"
+    :style="{'width':containerWidth+'px',
 	  'height':containerWidth/1.5+'px',
 	  'backgroundSize':containerWidth+'px '+containerWidth/1.5+'px',
 	  'position':'relative'
-	}">
+	}"
+  >
     <!-- home-btn -->
     <a
      href="#"
@@ -179,31 +16,50 @@
     'backgroundSize':containerWidth*89/2144+'px '+containerWidth*130/2144+'px',
     'width':containerWidth*69/2144+'px','height':containerWidth*130/2144+'px'}"
     ></a>
-    <div class="search clearfix"
-         :style="{'width':containerWidth*756/2144+'px','height':containerWidth*1230/2144+'px','position':'absolute','left':containerWidth*895/2144+'px','backgroundSize':containerWidth*756/2144+'px '+containerWidth*1230/2144+'px','top':0}">
-      <div class="left fl"
-           :style="{'width':containerWidth*52/2144+'px','height':containerWidth*995/2144+'px','backgroundSize':containerWidth*52/2144+'px '+containerWidth*995/2144+'px','marginTop':containerWidth*146/2144+'px'}">
-        <router-link :to="'/author/'+searchResult.logiciansId"
-                     :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*30/2144+'px','lineHeight':containerWidth*45/2144+'px'}"
-                     class="author-btn" v-show="searchResult.logiciansId">{{searchResult.logiciansName}}
+    <div
+        class="search clearfix"
+        :style="{'width':containerWidth*756/2144+'px','height':containerWidth*1230/2144+'px','position':'absolute','left':containerWidth*895/2144+'px','backgroundSize':containerWidth*756/2144+'px '+containerWidth*1230/2144+'px','top':0}">
+      <div
+        class="left fl"
+        :style="{'width':containerWidth*52/2144+'px','height':containerWidth*995/2144+'px','backgroundSize':containerWidth*52/2144+'px '+containerWidth*995/2144+'px','marginTop':containerWidth*146/2144+'px'}">
+        <router-link
+            :to="'/author/'+searchResult.logiciansId"
+            :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*30/2144+'px','lineHeight':containerWidth*45/2144+'px'}"
+            class="author-btn"
+            v-show="searchResult.logiciansId"
+        >
+            {{searchResult.logiciansName}}
         </router-link>
-        <button :disabled="isDisabled"
-                :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*360/2144+'px','lineHeight':containerWidth*70/2144+'px'}"
-                class="prev-btn" @click="prevSearch"></button>
-        <button :disabled="isDisabled"
-                :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*-10/2144+'px','lineHeight':containerWidth*70/2144+'px'}"
-                class="next-btn" @click="nextSearch"></button>
+        <button
+            :disabled="isDisabled"
+            :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*360/2144+'px','lineHeight':containerWidth*70/2144+'px'}"
+            class="prev-btn"
+            @click="prevSearch()">
+        </button>
+        <button
+            :disabled="isDisabled"
+            :style="{'width':containerWidth*52/2144+'px','height':containerWidth*195/2144+'px','marginTop':containerWidth*-10/2144+'px','lineHeight':containerWidth*70/2144+'px'}"
+            class="next-btn"
+            @click="nextSearch()"
+        ></button>
       </div>
-      <div class="center fl"
-           :style="{'width':containerWidth*480/2144+'px','height':containerWidth*1005/2144+'px','marginTop':containerWidth*140/2144+'px','backgroundSize':containerWidth*485/2144+'px '+containerWidth*1005/2144+'px'}"
-           :class="{searched:isSearched}">
-        <div class="search-result"
-             :style="{'width':containerWidth*480/2144+'px','height':containerWidth*1005/2144+'px','backgroundSize':containerWidth*485/2144+'px '+containerWidth*1005/2144+'px'}">
+      <div
+        class="center fl"
+        :style="{'width':containerWidth*480/2144+'px','height':containerWidth*1005/2144+'px','marginTop':containerWidth*140/2144+'px','backgroundSize':containerWidth*485/2144+'px '+containerWidth*1005/2144+'px'}"
+        :class="{searched:isSearched}"
+        >
+        <div
+            class="search-result"
+            :style="{'width':containerWidth*480/2144+'px','height':containerWidth*1005/2144+'px','backgroundSize':containerWidth*485/2144+'px '+containerWidth*1005/2144+'px'}"
+        >
           <ul class="stamp-list clearfix">
-            <li class="stamp-item fl" v-for="(item,index) in searchResult.stampList">
+            <li
+                class="stamp-item fl"
+                v-for="(item,index) in searchResult.stampList"
+            >
               <router-link :to="'/item/'+item.stampId">
                 <div class="top">
-                  <img :src="'https://api.duyin.ren/api/aliyun/oss/'+item.showImg" alt="">
+                  <img :src="'https://api.duyin.ren/api/aliyun/oss/'+item.showImg">
                 </div>
                 <p>{{item.chars}}</p>
               </router-link>
@@ -211,16 +67,20 @@
           </ul>
         </div>
         <!-- 名家列表 -->
-
-          <div id="authorList"  class="outer-box"
-               :style="{'height':containerWidth*432/2144+'px','bottom':containerWidth*57/2144+'px','left':containerWidth*432/2144+'px'}"
+          <div
+            id="authorList"
+            class="outer-box"
+            :style="{'height':containerWidth*432/2144+'px','bottom':containerWidth*57/2144+'px','left':containerWidth*432/2144+'px'}"
           >
             <div class="inner-box">
               <div class="author-list">
-                <div class="author-item" :data-sousuo="item.logiciansId"
-                     :class="{'active':authorId==item.logiciansId}" v-for="(item,index) in authorList"
-                     :style="{'width':containerWidth*60/2144+'px','height':containerWidth*216/2144+'px'}"
-                     @click="selectAuthor"> ⊙{{item.name}}
+                <div
+                    class="author-item"
+                    :data-sousuo="item.logiciansId"
+                    :class="{'active':authorId==item.logiciansId}"
+                    v-for="(item,index) in authorList"
+                    :style="{'width':containerWidth*60/2144+'px','height':containerWidth*216/2144+'px'}"
+                    @click="selectAuthor">⊙{{item.name}}
                 </div>
               </div>
             </div>
@@ -229,16 +89,24 @@
       <div class="right fl"
            :style="{'width':containerWidth*160/2144+'px','height':containerWidth*996/2144+'px','marginTop':containerWidth*146/2144+'px','backgroundSize':containerWidth*170/2144+'px '+containerWidth*996/2144+'px'}">
 
-        <textarea type="text" class="input-area" id="" v-model="chars"
-                  :style="{'width':containerWidth*50/2144+'px','height':containerWidth*430/2144+'px','marginTop':containerWidth*56/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*160/2144+'px '+containerWidth*1000/2144+'px'}"
-                  placeholder="输入文字"></textarea>
-        <button class="authorlist"
-                :style="{'width':containerWidth*108/2144+'px','height':containerWidth*216/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*60/2144+'px '+containerWidth*216/2144+'px'}"
-                @click="toggleAuthorList">列表
+        <textarea
+            type="text"
+            class="input-area"
+            v-model="chars"
+            :style="{'width':containerWidth*50/2144+'px','height':containerWidth*430/2144+'px','marginTop':containerWidth*56/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*160/2144+'px '+containerWidth*1000/2144+'px'}"
+            placeholder="输入文字"></textarea>
+        <button
+            class="authorlist"
+            :style="{'width':containerWidth*108/2144+'px','height':containerWidth*216/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*60/2144+'px '+containerWidth*216/2144+'px'}"
+            @click="toggleAuthorList()"
+        >列表
         </button>
-        <button class="searchbtn"
-                :style="{'width':containerWidth*60/2144+'px','height':containerWidth*216/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*60/2144+'px '+containerWidth*216/2144+'px'}"
-                @click.prevent="search">搜索
+        <button
+            class="searchbtn"
+            :style="{'width':containerWidth*60/2144+'px','height':containerWidth*216/2144+'px','marginLeft':containerWidth*15/2144+'px','backgroundSize':containerWidth*60/2144+'px '+containerWidth*216/2144+'px'}"
+            @click.prevent="search"
+        >
+            搜索
         </button>
       </div>
     </div>
@@ -261,7 +129,7 @@
         allNum: 0,
         lastNum: 0,
         chars: '',//关键字
-        searchResult: [],//
+        searchResult: [],
         preAllNum: 0,
         isSearched: false
         // authorSelect:false,//名家选择状态
@@ -272,13 +140,26 @@
       // this.search();
     },
     activated(){
-      // this.loadAuthorList();
+      // this.resetStatus();
+      // console.log(this.$route.path);
     },
     methods: {
+      // 重置搜索页
+      resetStatus(){
+        this.isSearched = false;
+        this.chars = ''
+        this.searchResult = [];
+        this.loadAuthorList();
+      },
       goBack(){
         window.history.back(-1)
       },
-      toggleAuthorList() {
+      toggleAuthorList(status) {
+        console.log(status);
+        if(status){
+          $('#authorList').animate({'width':0,'left':this.containerWidth*540/2144+'px'},500);
+          return;
+        }
         if(parseInt($('#authorList').css('width'))==0){
 	        $('#authorList').animate({
 	        	'width':this.containerWidth*540/2144+'px',
@@ -314,7 +195,7 @@
         this.search(this.allNum, this.lastNum);
       },
       prevSearch() {
-
+        console.log(this.preAllNum)
         if (this.preAllNum < 0) {
           this.preAllNum = 0;
           // this.isSearched = false;
@@ -324,6 +205,7 @@
         this.search(this.preAllNum, this.lastNum);
       },
       search(allNum, lastNum) {
+        console.log(allNum,lastNum)
         this.isDisabled = false;
         this.isShow = false;
         var url = common.apidomain + 'api/stamp/stampsQuery';
@@ -368,8 +250,7 @@
 
         // 选择名家、选择文字
         if (this.authorId !== -1 && this.chars.trim() !== '') {
-
-          var url = common.apidomain + 'api/stamp/stampsQuery';
+          // var url = common.apidomain + 'api/stamp/stampsQuery';
           // var data = {
           // 	logiciansId:this.authorId,
           // 	chars:this.chars,
@@ -377,12 +258,8 @@
           // 	lastNum:this.lastNum,
           // 	size:6
           // };
-          let formData = new FormData();
+          // let formData = new FormData();
           formData.append('logiciansId', this.authorId);
-          formData.append('chars', this.chars);
-          formData.append('allNum', this.allNum);
-          formData.append('lastNum', this.lastNum);
-          formData.append('size', 6);
           ajax(url, 'post', formData, (res) => {
             if (res.data.code != 200) {
               Toast(res.data.msg);
@@ -406,7 +283,7 @@
         if (this.authorId !== -1 && this.chars.trim() == '') {
           this.$router.push({path: '/author/' + this.authorId});
         }
-        this.toggleAuthorList();
+        this.toggleAuthorList(true);
       }
     },
     transitions: {
@@ -419,8 +296,183 @@
       containerWidth: function () {
         return window.innerWidth;
       }
+    },
+    watch:{
+      $route(newValue,oldValue) {
+        // console.log(newValue.path);
+        // console.log(oldValue.path);
+        if(newValue.path=='/search'&&oldValue.path=='/'){
+            console.log(1);
+        }
+      }
     }
   }
 
 </script>
+<style scoped>
+    .container {
+        box-sizing: border-box;
+    }
+
+    .search {
+        /*background-color: pink;*/
+        background: url('../../../static/images/search-bg.png') center center no-repeat;
+
+    }
+
+    .search .left {
+        /*width:.75rem;*/
+        /*height:1rem;*/
+        background: url('../../../static/images/result-left-bg.png') center center no-repeat;
+    }
+
+    .search .right {
+        text-align: center;
+        background: url('../../../static/images/search-box.png') center center no-repeat;
+    }
+
+    /*中间*/
+    .search .center {
+        position: relative;
+    }
+
+    .search .center.searched {
+        background: url('../../../static/images/search-bg2.png') center center no-repeat;
+    }
+
+    textarea {
+        padding-top: .2rem;
+    }
+
+    button {
+        color: transparent;
+    }
+
+    /*名家列表*/
+    .searchbtn {
+        background: url('../../../static/images/search-btn.png') center center no-repeat;
+        cursor: pointer;
+    }
+
+    /*搜索按钮*/
+    .authorlist {
+        background: url('../../../static/images/author.png') center center no-repeat;
+        cursor: pointer;
+    }
+
+    /*名家列表*/
+    .outer-box {
+        background-color: #baa99a;
+        position: absolute;
+        /*left: 0;*/
+        overflow: hidden;
+    }
+
+    .author-list {
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row-reverse;
+        flex-wrap: wrap;
+        overflow-y: scroll;
+        box-sizing: border-box;
+    }
+
+    /* for Chrome */
+    .author-list::-webkit-scrollbar {
+        display: none;
+    }
+
+    .author-item {
+        box-sizing: border-box;
+        padding-top: .2rem;
+        font-size: .3rem;
+        font-family: 'FangSong_GB2312';
+        border: 1px solid #fff;
+        margin-right: -1px;
+        margin-top: -1px;
+        cursor: pointer;
+        color: #fff;
+    }
+
+    .author-item.active {
+        color: #a38e70;
+        background-color: #fff;
+    }
+
+    .search-result {
+        /*background-color: rgba(0,0,0,.4);*/
+    }
+
+    .search-result .inner-box {
+        background-color: rgba(0, 0, 0, .4);
+    }
+
+    .search-result > ul {
+        height: 100%;
+    }
+
+    .stamp-item {
+        width: 33%;
+        height: 50%;
+        box-sizing: border-box;
+        font-size: 14px;
+    }
+
+    .stamp-item .top {
+        width: 100%;
+        height: 50%;
+        overflow: hidden;
+        box-sizing: border-box;
+        padding: 40px 20px;
+    }
+
+    .stamp-item img {
+        width: 100%;
+        display: block;
+        margin: 0 auto;
+    }
+
+    .stamp-item p {
+        width: 20%;
+        margin: 0px auto;
+        font-family: 'FangSong_GB2312';
+        font-size: 18px;
+        line-height: 30px;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 4;
+
+        overflow: hidden;
+
+        position:relative;
+        /*line-height:1.4em;*/
+        /* 3 times the line-height to show 3 lines */
+        height:112px;
+        overflow:hidden;
+    }
+    .stamp-item p::after {
+        content:"...";
+        font-weight:bold;
+        position:absolute;
+        bottom:0;
+        right:0;
+        padding:0 20px 1px 45px;
+    }
+
+    .input-area {
+        color: #a38e70;
+        font-family: 'FangSong_GB2312';
+    }
+
+    .home-btn {
+        background: url('../../../static/images/home-btn.png') no-repeat center center;
+        position: absolute;
+        z-index: 99999;
+        /*left: 0;*/
+    }
+</style>
 
